d0bbdf1036d43ebd38b0f1405538e68e10905f18
gpopowic <gmpopowich@gmail.com>
2024-01-25 11:12:50 -0800

    selected elements

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const name = document.querySelector("#name");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const terms = document.querySelector("#terms");
+const btnLogin = document.querySelector(".btn-login");
+const btnReset = document.querySelector(".btn-reset");
