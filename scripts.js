25c64d11f4768ff83f0aa87ee74faa9e8a053156
gpopowic <gmpopowich@gmail.com>
2024-01-25 12:09:23 -0800

    added rest of test code

---
+++ scripts.js
@@ -0,0 +1,165 @@
+const emailEl = document.querySelector("#email");
+const usernameEl = document.querySelector("#username");
+const passwordEl = document.querySelector("#password");
+const terms = document.querySelector("#terms");
+const btnLogin = document.querySelector(".btn-login");
+const btnReset = document.querySelector(".btn-reset");
+const form = document.querySelector(".form");
+
+//  add event listener to submit
+form.addEventListener("submit", function (e) {
+  // prevent the form from submitting
+  e.preventDefault();
+
+  // Validate forms
+  let isUsernameValid = checkUsername();
+  let isEmailValid = checkEmail();
+  let isPasswordValid = checkPassword();
+
+  let isFormValid = isUsernameValid && isEmailValid && isPasswordValid;
+
+  // submit to server if the form is valid
+  if (isFormValid) {
+  }
+});
+
+// check if form values are completed
+const isRequired = (value) => (value === "" ? false : true);
+
+//  checks to see if value is not between certain values
+const isBetween = (length, min, max) =>
+  length < min || length > max ? false : true;
+
+//   check if email is valid
+const isEmailValid = (email) => {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+};
+
+// check if password is strong enough
+const isPasswordSecure = (password) => {
+  const re = new RegExp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+  );
+  return re.test(password);
+};
+
+// show error messages if not entered correctly
+const showError = (input, message) => {
+  // get the form field element
+  const formField = input.parentElement;
+
+  // add the error class
+  formField.classList.remove("success");
+  formField.classList.add("error");
+
+  //show the error message
+  const error = formField.querySelector("small");
+  error.textContent = message;
+};
+
+// show success
+const showSuccess = (input) => {
+  // get the form-field element
+  const formField = input.parentElement;
+
+  // remove the error class
+  formField.classList.remove("error");
+  formField.classList.add("success");
+
+  // hide the error message
+  const error = formField.querySelector("small");
+  error.textContent = "";
+};
+
+// validate the username field
+const checkUsername = () => {
+  let valid = false;
+  const min = 3;
+  const max = 25;
+  const username = usernameEl.value.trim();
+
+  if (!isRequired(username)) {
+    showError(usernameEl, "Username cannot be blank.");
+  } else if (!isBetween(username.length, min, max)) {
+    showError(
+      usernameEl,
+      `Username must be between ${min} and ${max} characters.`
+    );
+  } else {
+    showSuccess(usernameEl);
+    valid = true;
+  }
+  return valid;
+};
+
+//Check the email field
+const checkEmail = () => {
+  let valid = false;
+  const email = emailEl.value.trim();
+  if (!isRequired(email)) {
+    showError(emailEl, "Email cannot be blank.");
+  } else if (!isEmailValid(email)) {
+    showError(emailEl, "Email is not valid.");
+  } else {
+    showSuccess(emailEl);
+    valid = true;
+  }
+  return valid;
+};
+
+// Validate password field
+const checkPassword = () => {
+  let valid = false;
+
+  const password = passwordEl.value.trim();
+
+  if (!isRequired(password)) {
+    showError(passwordEl, "Password cannot be blank.");
+  } else if (!isPasswordSecure(password)) {
+    showError(
+      passwordEl,
+      "Password must has at least 8 characters that include at least 1 lowercase character, 1 uppercase characters, 1 number, and 1 special character in (!@#$%^&*)"
+    );
+  } else {
+    showSuccess(passwordEl);
+    valid = true;
+  }
+
+  return valid;
+};
+
+const debounce = (fn, delay = 500) => {
+  let timeoutId;
+  return (...args) => {
+    // cancel the previous timer
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    // setup a new timer
+    timeoutId = setTimeout(() => {
+      fn.apply(null, args);
+    }, delay);
+  };
+};
+
+form.addEventListener(
+  "input",
+  debounce(function (e) {
+    switch (e.target.id) {
+      case "username":
+        checkUsername();
+        break;
+      case "email":
+        checkEmail();
+        break;
+      case "password":
+        checkPassword();
+        break;
+      case "confirm-password":
+        checkConfirmPassword();
+        break;
+    }
+  })
+);
